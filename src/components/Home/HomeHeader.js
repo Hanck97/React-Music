8256e19f084ddce3f20da49e3a7d79d672576151
HancockTong <1023537815@qq.com>
2018-05-14 20:00:19 +0800

    1

--- src/components/Home/HomeHeader.js
+++ src/components/Home/HomeHeader.js
@@ -1,6 +1,12 @@
+/*
+ * @Author: Hancock Tong 
+ * @Date: 2018-05-14 15:09:00 
+ */
+
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import classNames from 'classnames';
+
 
 
 class HomeHeader extends Component {
@@ -15,10 +21,17 @@ class HomeHeader extends Component {
                 </div>
                 <div className="searchBar">
                     <div className="searchInput">
-                        
+                        <i className="icon-search"></i>
+                        <input type="text" className="input input-search" onFocus={} placeholder="请输入关键词  " />                        
                     </div>
                 </div>
+                <div className="music-icon" >  
+                    <Link to={}>
+                        <div classNam="music-icon-animate" >
 
+                        </div>
+                    </Link>
+                </div>
 
             </div>
         )
---
+++ src/components/Home/Index.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: Hancock Tong 
+ * @Date: 2018-05-14 15:44:49 
+ */
+import React, {Component} from 'react';
+import HomeHeader from './HomeHeader'
+
+
+export default class extends Component {
+    componentDidMount(){
+
+    }
+
+    render() {
+        return (
+            <div>
+                <HomeHeader />
+            </div>
+        )
+
+    }
+
+}
